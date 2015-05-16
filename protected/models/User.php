22d96d3018cc0e590efb5fcca0dc2b4f00803108
warren <warren@openball.com>
2015-05-16 19:04:55 +0800

    new password update

--- protected/models/User.php
+++ protected/models/User.php
@@ -115,8 +115,8 @@ class User extends CActiveRecord
 
 	protected function beforeSave()
 	{
-		if(!empty($this->new_password))
-			$this->password = md5($this->new_password);
+		if(!empty($this->password))
+			$this->password = md5($this->password);
 
 		if($this->isNewRecord)
 			$this->create_time=$this->update_time=date("Y-m-d H:i:s");
