c2e4eb8c204bf007e56d26e494e90738fa7e63ef
warren <warren@openball.com>
2015-09-28 12:02:09 +0800

    test

--- protected/models/Hymn.php
+++ protected/models/Hymn.php
@@ -166,7 +166,10 @@ class Hymn extends CActiveRecord
 		$midi = CUploadedFile::getInstance($this,'midi');
 		$powerpoint = CUploadedFile::getInstance($this,'powerpoint');
         $path = Yii::app()->params['upload_dir'] . "/file/";
-		
+		print_r($notation);
+        print_r($midi);
+        print_r($powerpoint);
+        exit(0);
 		if (is_object($notation))
 		{
 			$notation->saveAs($path . 'hymn/notation/' . $this->id . "-notation" . $notation->getExtensionName());
